859369d7ebed7e124e71893c590ed8d228502772
Adrian <adriantate83@gmail.com>
2025-01-20 03:27:55 -0500

    update terraform cloud code

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "utrains"
+
+    workspaces {
+      name = "demo-repository"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = "~>1.5.0"
+}
